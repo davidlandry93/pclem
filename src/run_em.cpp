be1266520aa2045fc8a9d09c67b0fba2c897f3de
David Landry <davidlandry93@gmail.com>
2016-09-19 18:09:39 -0400

    Can now get the attributes from a reader

--- src/run_em.cpp
+++ src/run_em.cpp
@@ -1,6 +1,24 @@
 
 #include <iostream>
 
+#include <vtkPointSet.h>
+#include <vtkSmartPointer.h>
+#include <vtkSimplePointsReader.h>
+#include <vtkGenericDataObjectReader.h>
+
 int main(int argc, char** argv) {
     std::cout << "Hello world" << std::endl;
+
+    vtkSmartPointer<vtkGenericDataObjectReader> reader =
+        vtkSmartPointer<vtkGenericDataObjectReader>::New();
+
+    reader->SetFileName("res/bunny.vtk");
+    reader->Update();
+
+    vtkDataObject* data = reader->GetOutput();
+    vtkDataSetAttributes* attributes = data->GetAttributes(vtkDataObject::POINT);
+
+    std::cout << "output has " << data->GetDataObjectType() << " points." << std::endl;
+
+    return 0;
 }
