cfe8c9b9dd95d83d067451c4e138e8509b830f6f
David Landry <davidlandry93@gmail.com>
2016-12-16 14:53:10 -0500

    Log likelihood op is now parallel

--- pclem_lib/src/log_likelihood_operation.cu
+++ pclem_lib/src/log_likelihood_operation.cu
@@ -1,4 +1,7 @@
 
+#include <future>
+#include <thread>
+
 #include <glog/logging.h>
 #include <thrust/transform_reduce.h>
 
@@ -10,20 +13,22 @@ namespace pclem {
         mixture(mixture) {}
 
     double LogLikelihoodOperation::operator()(const DevicePointCloud::PointIterator& begin, const DevicePointCloud::PointIterator& end) const {
-        double log_likelihood = 0.0;
-
+        std::vector<std::future<double>> likelihoods;
         for(int i=0; i < AssociatedPoint::N_DISTRIBUTIONS_PER_MIXTURE; i++) {
             WeightedGaussian distribution = mixture.get_gaussian(i);
 
-            double likelihood_of_distribution = 0.0;
             if(distribution.get_weight() != 0.0) {
-                likelihood_of_distribution = log_likelihood_of_distribution(begin, end,
-                                                                            i, distribution);
+                std::packaged_task<double()> task([this,begin,end,distribution,i](){
+                        return log_likelihood_of_distribution(begin,end,i,distribution);
+                    });
+                likelihoods.push_back(task.get_future());
+                std::thread(std::move(task)).detach();
             }
+        }
 
-            VLOG(4) << likelihood_of_distribution;
-
-            log_likelihood += likelihood_of_distribution;
+        double log_likelihood = 0.0;
+        for(auto &likelihood : likelihoods) {
+            log_likelihood += likelihood.get();
         }
 
         return log_likelihood;
